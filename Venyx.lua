8245ea862240f21e29d36d5199424f94c2f915c3
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2023-01-06 04:41:09 +0100

    Update Venyx.lua

--- Venyx.lua
+++ Venyx.lua
@@ -14,12 +14,12 @@ local utility = {}
 -- themes
 local objects = {}
 local themes = {
-	Background = Color3.fromRGB(24, 24, 24), 
-	Glow = Color3.fromRGB(0, 0, 0), 
-	Accent = Color3.fromRGB(10, 10, 10), 
-	LightContrast = Color3.fromRGB(20, 20, 20), 
-	DarkContrast = Color3.fromRGB(14, 14, 14),  
-	TextColor = Color3.fromRGB(34, 55, 201)
+	Background = Color3.fromRGB(24, 24, 24),
+	Glow = Color3.fromRGB(190, 0, 0),
+	Accent = Color3.fromRGB(215, 0, 0),
+	LightContrast = Color3.fromRGB(20, 20, 20),
+	DarkContrast = Color3.fromRGB(14, 14, 14),
+	TextColor = Color3.fromRGB(255, 255, 255),
 }
 
 do
