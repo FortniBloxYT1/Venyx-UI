adbbfff61f2c853ae35b66654c2fc3f93fdc9f71
FortniBloxYT1 <98220426+FortniBloxYT1@users.noreply.github.com>
2023-04-09 05:23:43 +0200

    Update Venyx.lua

--- Venyx.lua
+++ Venyx.lua
@@ -186,6 +186,7 @@ do
 		input.InputChanged:Connect(function(input)
 			if input == dragInput and dragging then
 				local delta = input.Position - mousePos
+				task.wait(.1)
 				parent.Position  = UDim2.new(framePos.X.Scale, framePos.X.Offset + delta.X, framePos.Y.Scale, framePos.Y.Offset + delta.Y)
 			end
 		end)
